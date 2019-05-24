5c11702916b847df79b4d08fd5f9aa11562a98ab
kenilmavani <kenilmavani.scet@gmail.com>
2019-05-24 09:20:49 +0530

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,13 +1,13 @@
 import requests   					#import requests so we can call our API 
 import json       					#json for convert the output of API in list,dictionaries
 import datetime   					#it's for using datetime module for date format
-from dateutil import parser 		#using parser we can change ISO format into local format 
+from dateutil import parser 				#using parser we can change ISO format into local format 
 
-d2 = datetime.datetime.today() - datetime.timedelta(days=6)      # this date and time is 7 days ago date&time form current
+d2 = datetime.datetime.today() - datetime.timedelta(days=6)      	# this date and time is 7 days ago date&time form current
 d3 = datetime.datetime.today() - datetime.timedelta(days=1)		 # this date and time is 24 ago from current
 
 page=1				#this is page variable for next page of issuses 
-total_issues=0 		#Count of total issues in that project repository
+total_issues=0 			#Count of total issues in that project repository
 cnt_24=0			#count of open issues that were opened in the last 24 hours
 cnt_7=0				#count of open issues that were opened more than 24 hours ago but less than 7 days ago
 
@@ -24,26 +24,27 @@ url = "https://api.github.com/search/issues?q=repo:"+r[len(r)-2]+"/"+r[len(r)-1]
 #loop which going to last page of issues
 while True:
 	url=url+str(page)       			#adding page into URL for going on next page	
-	response = requests.get(url)		#getting responce from url
+	response = requests.get(url)			#getting responce from url
 	data = response.text				#data came from responce
 	parsed = json.loads(data)			#convert into lists,dictionaries
-	total_issues=parsed["total_count"]			#getting Total issues from that dictionary
-	if(len(parsed["items"])==0):		#if page is empty our while loop will break
+	total_issues=parsed["total_count"]		#getting Total issues from that dictionary
+	if(len(parsed["items"])==0):			#if page is empty our while loop will break
 		break
 	else :													
 		page+=1															#page will increase
 		b=0																#it's flag for if we get any issuse which opend 7 days ago it will 1 
 		for i in parsed["items"]:		
 			dt = parser.parse(i["created_at"]).replace(tzinfo=None)		#we get formal date from ISO date format	
-			if dt > d3 :												#comparing two dates one is from API another one is our
-				cnt_24+=1	 #incresing a count if issue were opened in the last 24 hours												
-			elif dt > d2 :  											
-				cnt_7+=1  	#incresing a count if issue were opened more than 24 hours ago but less than 7 days ago
+			
+			if dt > d3 : 	    #comparing two dates one is from API another one is our
+				cnt_24+=1   #incresing a count if issue were opened in the last 24 hours												
+			elif dt > d2 :  								
+				cnt_7+=1    #incresing a count if issue were opened more than 24 hours ago but less than 7 days ago
 			else:														
-				b=1			#here we get first issus which opened after 7 days ago so b = 1
-				break		#we break that for loop
+			     b=1	#here we get first issus which opened after 7 days ago so b = 1
+			     break	#we break that for loop
 		if(b==1):
-			break			#and also breaking our while loop too 
+			break	#and also breaking our while loop too 
 
 #total isuuses
 print(" Total issues:- ",total_issues)	
